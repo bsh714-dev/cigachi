79ad30dd297fa3d4c404117875c5d9d396f8386c
bsh714 <natebsh@nate.com>
2023-01-11 22:58:02 +0900

    hell

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export HOME="/home/ubuntu/"
-sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
-cd /home/ubuntu/project
-npm install
-npm run start:prod
+REPOSITORY=/home/ubuntu/build
+
+cd $REPOSITORY/project
+ls
+npm i
+
+# pm2 delete 0
+#NODE_ENV=production sudo pm2 start dist/src/main.js
